33f449b7e160ae6274fe2dbe8d723423846f0052
revalle88 <revalle88@gmail.com>
2018-01-09 12:11:37 +0300

    shoppingBackend init

---
+++ shoppingbackend/src/main/java/vpush/dev/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package vpush.dev.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
